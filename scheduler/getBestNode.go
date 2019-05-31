fce2fb02dbb595607eef40bca499ac310446b7f2
Meera Murali <mmurali@pdx.edu>
2019-05-30 18:57:43 -0700

    Added function to return node with name that matches the one returned from getBestNodeName()

---
+++ scheduler/getBestNode.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+)
+
+func getBestNode(nodes []Node) (Node, error) {
+    // Get name of best node per metrics
+    bestNodeName, err := getBestNodeName()
+    fmt.Printf("Best Node Name: %s\n", bestNodeName)
+    if err != nil {
+		return Node{}, err
+    }
+
+    // Initialize best node to nil
+    var bestNode *Node
+    bestNode = &Node{}
+
+    // Find actual node with that node
+	for _, n := range nodes {
+        // if name of node n matches bestNodeName
+        // save that as bestNode
+        nodeName := n.Metadata.Name
+        if nodeName == bestNodeName {
+            fmt.Printf("Found Best Node\n")
+            bestNode = &n
+            break
+        }
+	}
+
+    // return best node and error if any
+    if bestNode != nil {
+        return *bestNode, nil
+    } else {
+        return Node{}, fmt.Errorf("Unable to match best node name")
+    }
+}
+
